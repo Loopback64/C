3780983ad8f4c61867f587642f464ba5c246d3d5
loopback64 <luisheve@gmail.com>
2023-06-25 01:22:17 -0500

    Conversion temperatura farhenheit celsius punto flotante

---
+++ calculoTempoflotante.c
@@ -0,0 +1,41 @@
+/*Este programa sirve para calular la converison de temperaturas entre 
+ * grados celsius o punto flotante*/
+
+#include<stdio.h>
+/* imprime la tabla Farenheit-Celsius parafahr =0,20 ... 30; versión de punto flotante*/
+
+main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper =300;
+	step =20;
+
+
+	fahr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
