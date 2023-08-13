e14c6d1587b8a95c30815cda4d8fb56551d40134
loopback64 <luishever@gmail.com>
2023-08-12 23:00:31 -0500

    Determinar el resultado de la ejecucion del programa

---
+++ 10.-Ejercicio_resultado_ejecución_del_siguiente_programa.c
@@ -0,0 +1,27 @@
+// 
+
+#include<stdio.h>
+
+int main() {
+
+int x=7, y;
+   
+    y=-2+x;
+    
+    printf (" %d\n ", y);
+    
+    y=y+2;
+    
+    printf ( " %d\n ", y);
+    
+    y=(y==x);
+    
+    printf (" %d\n ", y);
+    
+    y++;
+    
+    printf (" %d\n ", y);
+}
+
+
+
