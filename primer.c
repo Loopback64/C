be870a6a85c8e228b813edf8edfde3582987de09
loopback64 <luishever@gmail.com>
2023-08-13 21:33:37 -0500

    Trabajando con variables

---
+++ primer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int numero = 10;
+    float numero_dec = 2.3;
+    double numero_dec2 = 5.1;
+    char letra = 'A';
+    char frase[] = "Hola";
+    printf("Hola\n");
+    return 0;
+
+}
+
+
+
+
+
