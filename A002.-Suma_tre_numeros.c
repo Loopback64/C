b1ea8457b0528595e6b69de5abba869e2149f3db
loopback64 <luishever@gmail.com>
2023-08-16 15:54:47 -0500

    suma de números

---
+++ A002.-Suma_tre_numeros.c
@@ -0,0 +1,18 @@
+// mostrar la suma de tres números siendo: A = 5, B = 8 y C = 1.
+
+
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    int A, B, C, suma;
+    A = 5;
+    B = 8;
+    C = 1;
+    suma = A + B + C;
+    printf("La suma es %i", suma);
+    return 0;
+}
