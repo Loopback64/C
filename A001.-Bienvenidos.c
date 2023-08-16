745aace1f52109fcc9eac049ba913ecf211c71f6
loopback64 <luishever@gmail.com>
2023-08-16 15:53:44 -0500

    Mensaje de bienbenida

---
+++ A001.-Bienvenidos.c
@@ -0,0 +1,15 @@
+//  mostrar el siguiente cartel “Bienvenidos a la Codificación”.
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Bienvenidos a la Codificación\n");
+    return 0;
+}
+
+
+
+
