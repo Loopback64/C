2ef5408a54fe92793316f6c3726940288b1f5a3e
loopback64 <luishever@gmail.com>
2023-09-14 23:59:21 -0500

    hallar la frecuencia de números de un dado, sabiendo que fueron 100 los tiros

---
+++ Ejemplo6.-HallarFrecuenciadenumerosdado.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+#define N 10
+
+int main()
+{
+
+int arreglo[N] , i ;
+
+for (i=0 ; i<N ; i++ )
+
+        {
+        arreglo[i]=i;
+        }
+printf("\n El vector resultante es : \n");
+for (i=0 ; i<N ; i++)
+        {
+                printf(" %i ", arreglo[i]);
+
+
+
+        }
+
+
+}
+
+
+
+
+
